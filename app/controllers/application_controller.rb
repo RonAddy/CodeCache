91b141b2441b341c5b6ffa86e6a518d17be2f68d
Aaron Bart-Addison <abartaddison12@gmail.com>
2017-12-12 21:25:01 -0500

    Created sessions controller, still working on application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  
+  # define sign in method in application controller so sign can be used anywhere in app
+  # token variable will be given random string
+  # Session object property "session_token" will be assigned the token 
+  # user session token will then be updated
+  def sign_in(user)
+	  token = SecureRandom.urlsafe_base64 #outputs random 22-char string
+	  session[:session_token] = token
+	  user.update_attribute(:session_token, token)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+  # new to render (GET) register form
+  # create to (POST) new session_token for user
+  # destroy (DELETE) to sign-out and delete session_token
+  resource :session, only: [:new, :create,  :destroy]
 end

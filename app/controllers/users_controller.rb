e82a6dbfd24953a861f90b28bb2821d9bd316b0f
Aaron Bart-Addison <abartaddison12@gmail.com>
2017-12-12 22:17:50 -0500

    Created user controller and added neccessary methods

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,39 @@
+class UsersController < ApplicationController
+	# will ensure user is signed out on the register form and when handling post request to create user
+  before_action :ensure_signed_out, only: [:new, :create]
+  before_action :ensure_signed_in, only: [:show, :index]
+
+  # will render register page to create a new user
+  def new
+    @user = User.new
+  end
+
+ # Will make post request if made user passes validations, and sign user in 
+  def create
+    @user = User.new(create_params)
+
+    if @user.save
+      sign_in(@user)
+      flash[:notice] = 'You are signed in!'
+      redirect_to users_path
+    else
+      flash[:error] = @user.errors.full_messages.join(', ')
+      render :new
+    end
+  end
+
+  def index
+    @users = User.all
+  end
+
+  def show
+	  #make this user profile page!
+    @user = User.find(params[:id])
+  end
+
+  private
+
+  def create_params
+    params.require(:user).permit(:username, :password)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
-  # new to render (GET) register form
+  resources :users, only: [:new, :create, :index, :show]
+  # new to render (GET) register/log-in form
   # create to (POST) new session_token for user
   # destroy (DELETE) to sign-out and delete session_token
+  
   resource :session, only: [:new, :create,  :destroy]
 end

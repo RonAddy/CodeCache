85e44230151912419bbbffd5642b412643058a68
Aaron Bart-Addison <abartaddison12@gmail.com>
2017-12-12 22:36:42 -0500

    Created root page for application...working on register page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
+  root to: 'sessions#new'
+  
   resources :users, only: [:new, :create, :index, :show]
   # new to render (GET) register/log-in form
   # create to (POST) new session_token for user

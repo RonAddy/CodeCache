fdfaf069aa3ee1e3dd5c43d5f20f280f93594883
Aaron Bart-Addison <abartaddison12@gmail.com>
2017-12-12 21:59:48 -0500

    Created application controller and sessions controller methods...working on users controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,4 +10,44 @@ class ApplicationController < ActionController::Base
 	  session[:session_token] = token
 	  user.update_attribute(:session_token, token)
   end
+  
+  # sign out method will return nill unless current_user is true (logged in)
+  # if ture, the current_user token will be updated to nil (session over)
+  # session token will be deleted on session object
+   def sign_out
+	    return unless current_user
+	    current_user.update!(session_token: nil)
+	    session.delete(:session_token)
+  end
+  
+  # current_user method made to prevent having to make database query evrytime we need the current user
+  # that query is made once with the find_current_user method
+  # @current_user will equal the user from first query or will be assigned current user if nil
+  def current_user
+    @current_user ||= find_current_user
+  end
+
+  def find_current_user
+    session_token = session[:session_token]
+    session_token && User.find_by(session_token: session_token)
+  end
+
+  # this method will make sure a user is logged in before allowing acces s to user specific pages
+  # will return nill if user is true (logged in)
+  # will display error message and redirect to root if the user is not logged in
+  # add "before_action :ensure_signed_in" in any controller that needs user to be logged in
+  
+  def ensure_signed_in
+    return if current_user
+    flash[:error] = 'you must be signed in to see this'
+    redirect_to :root
+  end
+
+  # for pages that need to be accessed by those who are not users (register page)
+  
+  def ensure_signed_out
+    return unless current_user
+    flash[:error] = 'you are already signed in'
+    redirect_to users_path
+  end
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,46 @@
 class SessionsController < ApplicationController
+	
+	# Will create a new instance of an User to pass through the new.html.erb view
+	
+	def new
+		@user = User.new
+		render :new
+	end
+	
+	# Username and password from user object will be taken from parameters
+	# A search for the user will be made using the username and password
+	# if true, a message will be displayed and user page will be rendered
+	# else, an error will display, a new user object will be made, and redirect to register page
+	# username attempt will still be displayed on redirect
+	def create 
+		username = user_params[:username]
+		password = user_params[:password]
+		
+		user = User.find_from_credentials(username, password)
+
+	    if user
+	       sign_in(user)
+	       flash[:notice] = "Hey, #{username}! "
+	       redirect_to users_path
+	    else
+	       flash[:error] = "User not found..."
+	       @user = User.new(username: username)
+	       render :new
+	    end
+	end
+	
+	#
+	def destroy
+		sign_out
+		flash[:notice] = 'You signed out!'
+		redirect_to new_session_path
+	end
+
+  
+	private
+	
+	def user_params
+		  params.require(:user).permit(:username, :password)
+	end
+  
 end

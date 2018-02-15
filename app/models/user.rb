a94edc453e6de605ce3f080c7e087dc4179f5ad7
Aaron Bart-Addison <abartaddison12@gmail.com>
2018-02-15 04:25:31 -0500

    Fixed some errors with comments

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,30 +8,28 @@ class User < ApplicationRecord
 	
 	#this will help to validate passwords before they are hashed and stored
 	# by saving the user input to the instance variable, the password will have to go through validation first
-	# allow nil for user inout that dows not need password input
+	# allow nil for user input that does not need password input
 	validates :password,  length: (8..20), allow_nil: true
-	
-	
 	
 	attr_reader :password
 	
-	# 	This method will allow me to check the password attempt
+	# This method will allow me to check the password attempt
 	# A new password object will be created(but not saved) comparing the current password_digest to the pasword attempt's digest
 	def is_password?(password_attempt)
 		BCrypt::Password.new(password_digest).is_password?(password_attempt)
 	end
 	
 	# The equal sign next to password will allow us to set a password value directly instead of having to pass it as a parameter
+	# Self within the method refers to every instance of a user
 	def password=(password)
 		@password = password
 		self.password_digest = BCrypt::Password.create(password)
 	end
 	
 	
-	# Self in method defineition refres to the class 
-	# self within the method refers to every instance of a user
-	# will check if the username is tue first
-	# then wil check if password attempt is equal to password_digets for user
+	# Self in method declaration refers to the class 
+	# will check if the username is true first
+	# then will check if password attempt is equal to password_diget for user
 	# will return nil(falsy) if any of those fail
 	def self.find_from_credentials(username, password)
 		user = find_by(username: username)
